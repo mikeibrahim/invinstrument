e22ab46fa1224bdb8a549a8e8d270dce3a8933be
Om Mahesh <2om.mahesh@gmail.com>
2024-01-27 19:59:34 -0800

    video (possibly)

--- src/pages/SelectInstrument/SelectInstrument.jsx
+++ src/pages/SelectInstrument/SelectInstrument.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import PIANO from './piano.png';
 import GUITAR from './guitar.png';
 import LEFT_ARROW from './left-arrow.svg';
+import Video from "../../components/Video";
 
 export default function SelectInstrument() {
     const [instrument, setInstrument] = useState("menu")
@@ -19,9 +20,12 @@ export default function SelectInstrument() {
         <title id="title">{instrument === "menu" ? "SELECT AN INSTRUMENT" : "PLAY"}</title>
         {instrument === "menu"
             ? <div id="instruments">
-                {instruments.map(data =>
-                    <img class="instrument-img" src={data.image} alt={data.name} onClick={() => setInstrument(data.name)} />)}
+                {instruments.map((data, i) =>
+                    <img className="instrument-img btn" key={i} src={data.image} alt={data.name} onClick={() => setInstrument(data.name)} />)}
             </div>
-            : <img src={LEFT_ARROW} alt="back" id="back" onClick={() => setInstrument("menu")}></img>}
+            : <>
+                <img src={LEFT_ARROW} alt="back" id="back" className="btn" onClick={() => setInstrument("menu")}></img>
+            </>}
+        <Video width={document.body.clientWidth} height={document.body.clientHeight} />
     </>
 }
