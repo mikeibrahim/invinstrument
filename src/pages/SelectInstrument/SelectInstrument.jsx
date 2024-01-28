8fd25513f0b6cd1c337cbc1295bb68ac84ffbdfa
Om Mahesh <2om.mahesh@gmail.com>
2024-01-27 18:57:15 -0800

    back button + css stuff

--- src/pages/SelectInstrument/SelectInstrument.jsx
+++ src/pages/SelectInstrument/SelectInstrument.jsx
@@ -1,16 +1,27 @@
-import React, { useRef, useState } from "react";
-import piano from './piano.png';
-import guitar from './guitar.png';
+import React, { useState } from "react";
+import PIANO from './piano.png';
+import GUITAR from './guitar.png';
+import LEFT_ARROW from './left-arrow.svg';
 
 export default function SelectInstrument() {
     const [instrument, setInstrument] = useState("menu")
+    const instruments = [
+        {
+            "name": "piano",
+            "image": PIANO
+        },
+        {
+            "name": "guitar",
+            "image": GUITAR
+        }
+    ]
     return <>
         <title id="title">{instrument === "menu" ? "SELECT AN INSTRUMENT" : "PLAY"}</title>
         {instrument === "menu"
             ? <div id="instruments">
-                <img src={piano} alt="piano" onClick={() => setInstrument("piano")} />
-                <img src={guitar} alt="guitar" onClick={() => setInstrument("guitar")} />
+                {instruments.map(data =>
+                    <img class="instrument-img" src={data.image} alt={data.name} onClick={() => setInstrument(data.name)} />)}
             </div>
-            : null}
+            : <img src={LEFT_ARROW} alt="back" onClick={() => setInstrument("menu")}></img>}
     </>
 }
