2daea1ae9c5914b513fb1657bfbe29a0cd3320ba
Om Mahesh <2om.mahesh@gmail.com>
2024-01-27 17:01:06 -0800

    started instrument select screen

--- src/pages/SelectInstrument/SelectInstrument.jsx
+++ src/pages/SelectInstrument/SelectInstrument.jsx
@@ -1,3 +1,16 @@
 import React, { useRef, useState } from "react";
+import piano from './piano.png';
+import guitar from './guitar.png';
 
-export default function SelectInstrument() {}
+export default function SelectInstrument() {
+    const [instrument, setInstrument] = useState("menu")
+    return <>
+        <title id="title">{instrument === "menu" ? "SELECT AN INSTRUMENT" : "PLAY"}</title>
+        {instrument === "menu"
+            ? <div id="instruments">
+                <img src={piano} alt="piano" onClick={() => setInstrument("piano")} />
+                <img src={guitar} alt="guitar" onClick={() => setInstrument("guitar")} />
+            </div>
+            : null}
+    </>
+}
